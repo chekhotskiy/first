13dc9cdc50e3e280784288ec5ef686154bf2a5fe
chekhotskiy <chekhotskiy@gmail.com>
2017-03-14 19:14:19 +0700

    добавлены функции

--- sandBox/src/main/java/program.java
+++ sandBox/src/main/java/program.java
@@ -1,9 +1,27 @@
 public class program {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		hello("world");
+		hello("bay");
 
-		System.out.println("hello? world");
+		double l = 5;
+		System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
+		double a = 5;
+		double b = 5;
+		System.out.println("площадь квадрата со стороной " + a + " и " + b + " = " + area(a, b));
+
 	}
 
-}
 
+	public static void hello(String somebody) {
+		System.out.println("hello? " + somebody + "!");
+	}
+
+	public static double area(double lem) {
+		return lem * lem;
+	}
+
+	public static double area(double a, double b) {
+		return a * b;
+	}
+}
